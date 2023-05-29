730c71db42a698ca9f2b78b90e3911be13572612
Fabian Ramos <fabian2103@gmail.com>
2023-05-29 14:11:20 -0500

    Add img in loader

--- src/components/Loadder.jsx
+++ src/components/Loadder.jsx
@@ -2,8 +2,14 @@ import './styles/Loadder.css'
 
 const Loadder = () => {
   return (
-    <div className="spinner">
-      <div className="spinnerin"></div>
+    <div className="absolute">
+      <div>
+        <img src="https://www.hindustantimes.com/ht-img/img/2023/02/27/550x309/sukuna_yuji_megumi_1677495818575_1677495829538_1677495829538.png" alt="" />
+      </div>
+
+      <div className="spinner">
+        <div className="spinnerin"></div>
+      </div>
     </div>
   )
 }
