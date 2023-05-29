613083663e76e42e87376faa726681aaf62ca307
Fabian Ramos <fabian2103@gmail.com>
2023-05-29 14:34:17 -0500

    Finish project 2

--- src/components/Loadder.jsx
+++ src/components/Loadder.jsx
@@ -2,12 +2,8 @@ import './styles/Loadder.css'
 
 const Loadder = () => {
   return (
-    <div className="absolute w-full h-[85%]">
-      <div>
-        <img src="https://www.hindustantimes.com/ht-img/img/2023/02/27/550x309/sukuna_yuji_megumi_1677495818575_1677495829538_1677495829538.png" alt="Not load img" className='object-cover h-[90%] w-full' />
-      </div>
-
-      <div className="flex justify-center items-center h-[15%]">
+    <div className="absolute w-full h-full">
+      <div className='bg-[url("https://www.hindustantimes.com/ht-img/img/2023/02/27/550x309/sukuna_yuji_megumi_1677495818575_1677495829538_1677495829538.png")] bg-cover w-full h-full'>
         <div className="spinner">
           <div className="spinnerin"></div>
         </div>
